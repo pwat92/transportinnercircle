14f7d01f18d8915d4e54fded1c53d666a6f7e5e6
pwat92 <patrick.watson92@gmail.com>
2016-06-26 14:14:07 -0400

    enable creation of new admins once signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  resources :new_driver, :only => [:new, :create]
+  root 'static_pages#index'
 
+  resources :new_driver, :only => [:new, :create]
   resources :quotes, :only => [:new, :create]
 
   mount RailsAdmin::Engine => '/admin_panel', as: 'rails_admin'
-  devise_for :admins
-  root 'static_pages#index'
+  
+  devise_for :admins,
+             :controllers => {
+               :registrations => "registrations"
+             }
+  
+  as :admin do
+    get "/register", to: "registrations#new", as: "register"
+  end
 
   get '/track_shipment', to: 'static_pages#track_shipment'
   get '/photos', to: 'static_pages#photos'
